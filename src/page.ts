c894f141798c48e458e6b95a33899d6d16084e05
huangsiqin <450555286@qq.com>
2020-07-29 14:31:37 +0800

    泛型

--- src/page.ts
+++ src/page.ts
@@ -1,7 +1,23 @@
-import $ from 'jquery'
+interface Person {
+    name: string,
+    age: number,
+    gender: string
+}
 
-$(function () {
-    $('body').html('<div>123</div>')
-    new $.fn.init()
+class Teacher {
+    constructor (private info:Person) {}
+    getInfo <T extends keyof Person>(key: T): Person[T] {
+        return this.info[key]
+    }
+
+}
+
+const teacher = new Teacher ({
+    name: 'hsq',
+    age: 25,
+    gender: 'male'
 })
 
+const test = teacher.getInfo('age')
+
+console.log(teacher.getInfo('gender'))
